bba2fdedf8dd8800d0067317365ee6eb575c76de
abdelrhman marzoq <abdelrahmann595@gmail.com>
2024-10-17 23:20:11 +0300

    I2C_Driver

--- Drivers/STM32F103C6_MCAL_Drivers/I2C/STM32F103C6_I2C_Driver.c
+++ Drivers/STM32F103C6_MCAL_Drivers/I2C/STM32F103C6_I2C_Driver.c
@@ -6,3 +6,411 @@
  * 			Created on: Oct 10, 2024
  ******************************************************************************
 **/
+
+#include "STM32F103C6_I2C_Driver.h"
+
+
+/*
+ * =======================================================================================
+ * 										Generic Variables
+ * =======================================================================================
+*/
+
+//#define Polling(X, EV) while(!Get_Status(X, EV))
+
+typedef enum
+{
+	Busy,
+	EV5,
+	EV6,
+	EV8_1,
+	EV8,
+	EV8_2,
+	EV7
+
+
+}Flag_Type;
+
+typedef enum
+{
+	RESET,
+	SET
+}Status;
+
+
+/*
+ * =======================================================================================
+ * 										Generic Functions
+ * =======================================================================================
+*/
+static void I2C_GPIO_PINs(I2C_Periphral *I2Cx);
+
+static void Generate_Start(I2C_Periphral *I2Cx, Start_Cond Start);
+
+static void Generate_Stop(I2C_Periphral *I2Cx);
+
+static Status Get_Status(I2C_Periphral *I2Cx, Flag_Type Flag);
+
+
+
+
+
+/*
+ * =======================================================================================
+ * 										APIs
+ * =======================================================================================
+*/
+
+
+/*The following is the required sequence in master mode.
+* 1- Program the peripheral input clock in I2C_CR2 Register in order to generate correct timings
+* 2- Configure the clock control registers
+* 3- Configure the rise time register
+* 4- Program the I2C_CR1 register to enable the peripheral
+*/
+
+void I2C_INIT(I2C *I2C_Conf)
+{
+	uint32_t PCLK = 0, FREQ = 0;
+	if (I2C_Conf->I2Cx == I2C1)
+	{
+		RCC_I2C1_CLK_EN;
+		I2C_GPIO_PINs(I2C1);
+	}
+	else if (I2C_Conf->I2Cx == I2C2)
+	{
+		RCC_I2C2_CLK_EN;
+		I2C_GPIO_PINs(I2C2);
+	}
+
+	/*1- Program the peripheral input clock in I2C_CR2 Register in order to generate correct timings
+	* Bits 5:0 FREQ[5:0]: Peripheral clock frequency in CR2
+	* The minimum allowed frequency is 2 MHz,
+	* the maximum frequency is limited by the maximum APB frequency and cannot exceed
+	* 50 MHz (peripheral intrinsic maximum limit).
+	* 0b000000: Not allowed
+	* 0b000001: Not allowed
+	* 0b000010: 2 MHz
+	* ...
+	* 0b110010: 50 MHz
+	* Higher than 0b100100: Not allowed
+	*/
+	PCLK = RCC_GetPCLK1Freq();
+	FREQ = PCLK / 1000000;
+	I2C_Conf->I2Cx->CR2 &= ~(0b111111 << FREQ_BITS_Pos);
+	I2C_Conf->I2Cx->CR2 |= FREQ << FREQ_BITS_Pos;
+
+
+	/*2- Configure the clock control registers
+	* The CCR register must be configured only when the I2C is disabled (PE = 0).
+	* Thigh = CCR * Tpclk
+	* Ti2c = Thigh + Tlow
+	* Thigh = Tlow (Duty cycle 50%)
+	* CCR = Thigh / Tpclk || (Ti2c/2) / Tpclk || Fpclk / (Fi2c*2)
+	* CCR = Fpclk / (Fi2c*2)
+	*/
+
+	// Clear PE Bit to config CCR Reg
+	I2C_Conf->I2Cx->CR1 &= ~(0b1 << PE_BIT_Pos);
+
+	if (I2C_Conf->Mode == SM_Mode)
+	{
+		// Enable SM_Mode
+		I2C_Conf->I2Cx->CR1 &= ~(0b1 << F_S_BIT_Pos);
+		I2C_Conf->I2Cx->CCR = ((PCLK / (I2C_Conf->SCL_Speed << 1)) & 0xFFF) << CRR_BITS_Pos;
+	}
+	else
+	{
+		// To Do
+	}
+
+	/*3- Configure the rise time register
+	 * FREQ + 1
+	 */
+	I2C_Conf->I2Cx->TRISE = (((I2C_Conf->I2Cx->CR2 << FREQ_BITS_Pos) & 0x3F) + 1 ) & 0x3F << TRISE_BITS_Pos;
+
+	// Set PE Bit to Enable I2C Periphral
+	I2C_Conf->I2Cx->CR1 |= (0b1 << PE_BIT_Pos);
+}
+
+void I2C_Slave_Init (I2C *I2C_Conf, Slave *Address)
+{
+	if (Address->Mode == Bit_7)
+	{
+		I2C_Conf->I2Cx->OAR1 &= ~(0b1 << ADD_MODE_BIT_POS);
+		if (Address->Dual == ENABLE)
+		{
+			I2C_Conf->I2Cx->OAR2 |= (0b1 << ENDUAL_BIT_POS);
+			I2C_Conf->I2Cx->OAR2 |= ((Address->Address2 & (0x7F))  << ADD2_BITS_POS);
+		}
+		I2C_Conf->I2Cx->OAR1 |= ((Address->Address1 & (0x7F)) << ADD_1_7_BITS_POS);
+	}
+	else if (Address->Mode == Bit_10)
+	{
+//		To Do
+	}
+}
+
+
+
+void I2C_Master_Write(I2C *I2C_Conf, Address_Mode Mode, uint16_t devAddr, uint8_t *data, uint32_t dataLength, Stop_Cond Stop,Start_Cond Start)
+{
+//	Generate Start According to Start_Condition (Normal Start with Stop or Repeated Start )
+	Generate_Start(I2C_Conf->I2Cx, Start);
+
+	if (Mode == Bit_7)
+	{
+		uint32_t i = 0;
+//		Polling Untill Start Bit Is generated Succesfully
+//		EV5: SB=1, cleared by reading SR1 register followed by writing DR register with Address.
+		while(!Get_Status(I2C_Conf->I2Cx, EV5));
+
+		// Send Slave Address on Bus
+		I2C_Conf->I2Cx->DR =  ((devAddr  & 0x7F) << 1) | Writing_Operation;
+
+//		Polling till Address Sent
+//		EV6: ADDR=1, cleared by reading SR1 register followed by reading SR2.
+		while(!Get_Status(I2C_Conf->I2Cx, EV6));
+//		Polling Till DR empty
+//		EV8_1: TxE=1, shift register empty, data register empty, write Data1 in DR.
+		while(!Get_Status(I2C_Conf->I2Cx, EV8_1));
+
+		for (i = 0; i < dataLength; i++)
+		{
+			I2C_Conf->I2Cx->DR =  *data;
+			while(!Get_Status(I2C_Conf->I2Cx, EV8));
+			data++;
+		}
+//		EV8_2: TxE=1, BTF = 1, Program Stop request. TxE and BTF are cleared by hardware by the Stop condition
+//		(Option to Ensure that there is no Data in DR) (I Will not terminate For loop till I Send all data )
+//		while(!Get_Status(I2C_Conf->I2Cx, EV8_2))
+
+//		Generate Stop Bit if Start isn`t Repeated
+		if (Stop != Without_Stop)
+		{
+			Generate_Stop(I2C_Conf->I2Cx);
+		}
+	}
+	/* To Do In Future 10 Bit Address Mode */
+	else if (Mode == Bit_10)
+	{
+//		To Do
+	}
+}
+
+
+void I2C_Master_Read(I2C *I2C_Conf, Address_Mode Mode, uint16_t devAddr, uint8_t *dataOut, uint32_t dataLength, Stop_Cond Stop,Start_Cond Start)
+{
+//	Enable Ack Bit
+	I2C_Conf->I2Cx->CR1 |= 0b1 << ACK_BIT_POS;
+
+	Generate_Start(I2C_Conf->I2Cx, Start);
+
+	if (Mode == Bit_7)
+	{
+		while(!Get_Status(I2C_Conf->I2Cx, EV5));
+
+//		Set Device Address at DR Reg
+		I2C_Conf->I2Cx->DR |= ((devAddr  & 0x7F) << 1);
+//		Clearing First Bit Reading Operation
+		I2C_Conf->I2Cx->DR &= ~(0b1 << 0);
+
+//		Polling till Address Sent
+//		EV6: ADDR=1, cleared by reading SR1 register followed by reading SR2.
+		while(!Get_Status(I2C_Conf->I2Cx, EV6));
+
+		if (dataLength)
+		{
+			for (uint32_t i = 0; i < dataLength; i++)
+			{
+//				EV7: RxNE = 1 Cleared by Reading DR Reg
+				while(!Get_Status(I2C_Conf->I2Cx, EV6));
+				*dataOut = I2C_Conf->I2Cx->DR;
+				dataOut++;
+			}
+		}
+
+//		Disable Ack Bit
+		I2C_Conf->I2Cx->CR1 &= ~(0b1 << ACK_BIT_POS);
+
+		if (Stop != Without_Stop)
+		{
+			Generate_Stop(I2C_Conf->I2Cx);
+		}
+	}
+	/* To Do In Future 10 Bit Address Mode */
+	else if (Mode == Bit_10)
+	{
+//		To Do
+	}
+
+}
+
+
+void I2C_deINIT(I2C *I2C_Conf)
+{
+	if (I2C_Conf->I2Cx == I2C1)
+	{
+		RCC_I2C1_CLK_DIS;
+		NVIC_Disable_ER_I2C1;
+		NVIC_Disable_EV_I2C1;
+	}
+	else if (I2C_Conf->I2Cx == I2C2)
+	{
+		RCC_I2C2_CLK_DIS;
+		NVIC_Disable_ER_I2C2;
+		NVIC_Disable_EV_I2C2;
+	}
+}
+
+/*
+ * =======================================================================================
+ * 										Generic Functions
+ * =======================================================================================
+*/
+
+
+static void I2C_GPIO_PINs(I2C_Periphral *I2Cx)
+{
+	// Setting Pins from recommended As Mentioned in Data Sheet
+	GPIO_PinConfig_t I2C_Pins;
+	if (I2Cx == I2C1)
+	{
+		//SCL : PB6
+		I2C_Pins.GPIOx = GPIOB;
+		I2C_Pins.GPIO_MODE = GPIO_MODE_OUTPUT_AF_OD;
+		I2C_Pins.GPIO_OUTPUT_Speed = GPIO_SPEED_2M;
+		I2C_Pins.GPIO_PinNumber = GPIO_PIN6;
+		GPIO_INIT(&I2C_Pins);
+
+		//SDA : PB7
+		I2C_Pins.GPIOx = GPIOB;
+		I2C_Pins.GPIO_MODE = GPIO_MODE_OUTPUT_AF_OD;
+		I2C_Pins.GPIO_OUTPUT_Speed = GPIO_SPEED_2M;
+		I2C_Pins.GPIO_PinNumber = GPIO_PIN7;
+		GPIO_INIT(&I2C_Pins);
+	}
+	else if (I2Cx == I2C2)
+	{
+		//SCL : PB10
+		I2C_Pins.GPIOx = GPIOB;
+		I2C_Pins.GPIO_MODE = GPIO_MODE_OUTPUT_AF_OD;
+		I2C_Pins.GPIO_OUTPUT_Speed = GPIO_SPEED_2M;
+		I2C_Pins.GPIO_PinNumber = GPIO_PIN10;
+		GPIO_INIT(&I2C_Pins);
+
+		//SDA : PB11
+		I2C_Pins.GPIOx = GPIOB;
+		I2C_Pins.GPIO_MODE = GPIO_MODE_OUTPUT_AF_OD;
+		I2C_Pins.GPIO_OUTPUT_Speed = GPIO_SPEED_2M;
+		I2C_Pins.GPIO_PinNumber = GPIO_PIN11;
+		GPIO_INIT(&I2C_Pins);
+	}
+}
+
+
+static void Generate_Start(I2C_Periphral *I2Cx, Start_Cond Start)
+{
+	if (Start != Repeated)
+	{
+		// Polling on the bus Untill The Bus not busy
+		while( !(Get_Status(I2Cx, Busy)) );
+	}
+
+//	Bit 8 START: Start generation
+//	This bit is set and cleared by software and cleared by hardware when start is sent or PE=0.
+//	In Master Mode:
+//	0: No Start generation
+//	1: start generation
+
+	I2Cx->CR1 |= 0b1 << START_BIT_Pos;
+
+}
+
+static void Generate_Stop(I2C_Periphral *I2Cx)
+{
+//	Bit 9 STOP: Stop generation
+//	The bit is set and cleared by software, cleared by hardware when a Stop condition is
+//	detected, set by hardware when a timeout error is detected.
+//	In Master Mode:
+//	0: No Stop generation.
+//	1: Stop generation after the current byte transfer or after the current Start condition is sent
+
+	I2Cx->CR1 |= 0b1 << STOP_BIT_Pos;
+}
+
+static Status Get_Status(I2C_Periphral *I2Cx, Flag_Type Flag)
+{
+	Status bitStatus = RESET;
+	uint32_t dummyRead;
+	switch(Flag)
+	{
+	case Busy :
+	{
+		if (I2Cx->SR2 & 0b1 << BUSY_BIT_Pos)
+			bitStatus = SET;
+		else
+			bitStatus = RESET;
+		break;
+	}
+	case EV5:
+	{
+		if (I2Cx->SR1 & 1 << SB_BIT_Pos)
+		{
+			bitStatus = SET;
+			dummyRead = I2Cx->SR1;
+		}
+		else
+			bitStatus = RESET;
+		break;
+	}
+	case EV6:
+	{
+		if (I2Cx->SR1 & (0b1 << ADDR_BIT_Pos))
+		{
+			bitStatus = SET;
+			dummyRead = I2Cx->SR1;
+			dummyRead = I2Cx->SR2;
+		}
+		else
+			bitStatus = RESET;
+		break;
+	}
+	case EV8_1:
+	{
+		if ( (I2Cx->SR1 & (1 << TxE_BIT_Pos)) && (I2Cx->SR2 & (0b111 << 0)) )
+			bitStatus = SET;
+		else
+			bitStatus = RESET;
+		break;
+	}
+
+	case EV8:
+	{
+		if (I2Cx->SR1 & (1 << TxE_BIT_Pos))
+			bitStatus = SET;
+		else
+			bitStatus = RESET;
+		break;
+	}
+	case EV8_2:
+	{
+		if ( (I2Cx->SR1 & (1 << TxE_BIT_Pos)) && ((I2Cx->SR1 & 1) << BTF_BIT_Pos) )
+			bitStatus = SET;
+		else
+			bitStatus = RESET;
+		break;
+	}
+	case EV7:
+	{
+		if (I2Cx->SR1 & 1 << RxNE_BIT_Pos)
+			bitStatus = SET;
+		else
+			bitStatus = RESET;
+		break;
+	}
+
+	}
+
+	return bitStatus;
+}
--- Drivers/STM32F103C6_MCAL_Drivers/I2C/STM32F103C6_I2C_Driver.h
+++ Drivers/STM32F103C6_MCAL_Drivers/I2C/STM32F103C6_I2C_Driver.h
@@ -7,15 +7,202 @@
  ******************************************************************************
 **/
 
-
 #ifndef _I2C_DRIVER_H_
 #define _I2C_DRIVER_H_
 
 
+#include "../STM32F103X6.h"
+#include "../GPIO/STM32F103C6_GPIO_Driver.h"
+#include "../RCC/STM32F103C6_RCC_Driver.h"
 
 
 
 
+#define Writing_Operation   1
+#define MASTER_BYTE_TRANSMIT 0x00070080
+
+
+
+//        CR1 Reg Bits Pos
+#define PE_BIT_Pos       	0
+#define START_BIT_Pos		8
+#define STOP_BIT_Pos		9
+#define ACK_BIT_POS			10
+
+//        CR2 Reg Bits Pos
+#define FREQ_BITS_Pos    	0
+
+//        CRR Reg Bits Pos
+#define F_S_BIT_Pos      	15
+#define CRR_BITS_Pos     	0
+
+//        TRISE Reg Bits Pos
+#define TRISE_BITS_Pos	 	0
+
+//        OAR1 Reg Bits Pos
+#define ADD_10_BITS_POS     0
+#define ADD_1_7_BITS_POS    1
+#define ADD_MODE_BIT_POS 	15
+
+//        OAR2 Reg Bits Pos
+#define ENDUAL_BIT_POS      0
+#define ADD2_BITS_POS	    1
+
+//        SR1 Reg Bits Pos
+#define SB_BIT_Pos		    0
+#define ADDR_BIT_Pos		1
+#define BTF_BIT_Pos			3
+#define RxNE_BIT_Pos		6
+#define TxE_BIT_Pos  		7
+
+
+//        SR2 Reg Bits Pos
+#define BUSY_BIT_Pos        1
+
+
+
+
+
+
+
+/**********************************  References  *************************************/
+
+// @ref I2Cx
+// I2C1					For using I2C1
+// I2C2					For using I2C2
+
+
+// @ref I2C_Mode
+typedef enum
+{
+	SM_Mode,                    // I2C Standard Mode Clk Up to 100KHz   (Recommended)
+	FM_Mode						// I2C Fast Mode CLK up to 400KHz		(Not Supported in Driver)
+}I2C_Mode;
+
+// @ref SCL_Speed
+typedef enum
+{
+	SCL_50KHz 	= 50000,                    	// I2C SCL Bus Speed 50KHz							For SM Mode
+	SCL_100KHz 	= 100000,						// I2C SCL Bus Speed 100KHz			(Recommended)	For SM Mode
+	/******************************* FM Mode not supported in driver *******************************/
+	SCL_200KHz 	= 200000,						// I2C SCL Bus Speed 200KHz							For FM Mode
+	SCL_400KHz 	= 400000						// I2C SCL Bus Speed 400KHz							For FM Mode
+}SCL_Speed;
+
+
+// @ref Address_Mode
+typedef enum
+{
+	Bit_7,
+	Bit_10
+}Address_Mode;
+
+// @ref Dual_EN
+typedef enum
+{
+	DISABLE,
+	ENABLE
+}Dual_EN;
+
+
+typedef enum
+{
+	Start,
+	Repeated
+}Start_Cond;
+
+typedef enum
+{
+	Without_Stop,
+	With_Stop
+}Stop_Cond;
+
+
+/***********************************************************************/
+
+
+/* Implement it if you need MicroController Act as Slave not Master */
+typedef struct
+{
+	 Address_Mode Mode;				// Choosing Address Mode based on @ref Address_Mode
+	 Dual_EN Dual;					// Enable & Disable Dual Address based on @ref Dual_EN
+	 uint16_t Address1;				// Specify First Address
+	 uint16_t Address2;				// Specify Second Address if Dual is Enabled
+}Slave;
+
+typedef struct
+{
+	I2C_Periphral *I2Cx;					// Specifies Which I2C Use.
+											// This parameter must be set based on @ref I2Cx
+
+	I2C_Mode	   Mode;					// Specifies Which I2C Mode.
+											// This parameter must be set based on @ref I2C_Mode
+
+	SCL_Speed	   SCL_Speed;				// Specifies SCL Bus Speed.
+											// This parameter must be set based on @ref SCL_Speed
+
+}I2C;
+
+
+
+
+/*
+ * =======================================================================================
+ * 										APIs
+ * =======================================================================================
+*/
+
+/**================================================================
+ * @Fn                 	- I2C_INIT
+ * @brief				- Init I2C Periphral
+ *  					  Must Call First Befor any Sending or Recieving Data
+ * @param [in]			- Pointer to I2C
+ *
+ * @retval				- NONE
+ * Note					- NONE
+ *================================================================*/
+void I2C_INIT(I2C *I2C_Conf);
+
+/**================================================================
+ * @Fn                 	- I2C_Slave_Init
+ * @brief				- Init MCU as Slave
+ * @param [in]			- Pointer to I2C , Pointer to struct Include Address to act as Slave
+ *
+ * @retval				- NONE
+ * Note					- NONE
+ *================================================================*/
+void I2C_Slave_Init (I2C *I2C_Conf, Slave *Address);
+
+
+/**================================================================
+ * @Fn                 	- I2C_Master_Write
+ * @brief				-
+ * @param [in]			-
+ *
+ * @retval				- NONE
+ * Note					- NONE
+ *================================================================*/
+void I2C_Master_Write(I2C *I2C_Conf, Address_Mode Mode, uint16_t devAddr, uint8_t *data, uint32_t dataLength, Stop_Cond Stop,Start_Cond Start);
+
+/**================================================================
+ * @Fn                 	- I2C_Master_Read
+ * @brief				-
+ * @param [in]			-
+ *
+ * @retval				- NONE
+ * Note					- NONE
+ *================================================================*/
+void I2C_Master_Read(I2C *I2C_Conf, Address_Mode Mode, uint16_t devAddr, uint8_t *dataOut, uint32_t dataLength, Stop_Cond Stop, Start_Cond Start);
+
+/**================================================================
+ * @Fn                 	- I2C_deINIT
+ * @brief				- De Init I2C Bus (Free Pins)
+ * @param [in]			- Pointer to I2C
+ *
+ * @retval				- NONE
+ * Note					- NONE
+ *================================================================*/
+void I2C_deINIT(I2C *I2C_Conf);
 
 
 #endif
