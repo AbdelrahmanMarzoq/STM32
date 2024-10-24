e9583c94718993ff518c675bcad7720249e396b7
abdelrhman marzoq <abdelrahmann595@gmail.com>
2024-10-24 15:15:36 +0300

    SPI Driver

--- Drivers/STM32F103C6_MCAL_Drivers/SPI/STM32F103C6_SPI_Driver.c
+++ Drivers/STM32F103C6_MCAL_Drivers/SPI/STM32F103C6_SPI_Driver.c
@@ -6,3 +6,341 @@
  *
  ******************************************************************************
 **/
+
+
+#include "STM32F103C6_SPI_Driver.h"
+
+
+
+/*
+ * =======================================================================================
+ * 										Generic Variables
+ * =======================================================================================
+*/
+
+
+
+/*
+ * =======================================================================================
+ * 										Generic Functions
+ * =======================================================================================
+*/
+
+static void SPI_GPIO_PINs(SPI *SPI_Conf);
+
+
+
+/*
+ * =======================================================================================
+ * 										APIs
+ * =======================================================================================
+*/
+
+void SPI_MASTER_INIT(SPI *SPI_Conf)
+{
+	uint16_t tempCR1 = 0;
+	uint8_t  tempCR2  = 0;
+
+	if (SPI_Conf->SPIx == SPI1)
+	{
+		RCC_SPI1_CLK_EN;
+	}
+	else if (SPI_Conf->SPIx == SPI2)
+	{
+		RCC_SPI2_CLK_EN;
+	}
+
+	// Enable Periphral
+	tempCR1 |= (1 << SPE_BIT_Pos);
+
+	// Identify Master Or Slave
+	if (SPI_Conf->Master_Slave_Mode == SPI_Slave)
+		tempCR1 &= ~(1 << MSTR_BIT_Pos);
+	else if (SPI_Conf->Master_Slave_Mode == SPI_Master)
+		tempCR1 |= (1 << MSTR_BIT_Pos);
+
+	//Clearing LSBFIRST Bit this Mean in Frame Will Send MSB First
+	tempCR1 &= ~(1 << LSBFIRST_BIT_Pos);
+
+	// Identify Frame Size of Data Transmit and recieved
+	if ( SPI_Conf->Frame_Size == SPI_BIT_8 )
+		tempCR1 &= ~(1 << DFF_BIT_Pos);    	// Frame of Data will be 8  Bit
+	else if (SPI_Conf->Frame_Size == SPI_BIT_16)
+		tempCR1 |= (1 << DFF_BIT_Pos);		// Frame of Data will be 16 Bit
+
+	// Set Freq Of SCLK
+	tempCR1 |= SPI_Conf->Prescaller << BR_BITS_Pos;
+
+	// Idle CLK Is One
+	tempCR1 |= 1 << CPOL_BIT_Pos;
+
+	// The Second Edge of CLK Capture The Data
+	tempCR1 |= 1 << CPHA_BIT_Pos;
+
+	// Init SPI Communication Mode
+	if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+	{
+		// Not Supported (To Do)
+		// Half Duplex interaccte with MISO Pin
+		// Bidirectional Recieve Only
+		tempCR1 |= 1 << BIDIMODE_BIT_Pos;
+		tempCR1 &= ~(1 << BIDIOE_BIT_Pos);
+	}
+	else if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+	{
+		// Full Duplex Mode Enable MOSI and MISO
+		tempCR1 &= ~(1 << BIDIMODE_BIT_Pos);
+		tempCR1 &= ~(1 << RXONLY_BIT_Pos);
+	}
+
+	// NSS SW (Not Supported HW)
+	tempCR1 |= 1 << SSM_BIT_Pos;
+
+
+
+	// En Or Dis IRQ
+	if (SPI_Conf->IRQ != SPI_IRQ_NONE)
+	{
+		tempCR2 |= SPI_Conf->IRQ;
+		if (SPI_Conf->SPIx == SPI1)
+			NVIC_Enable_SPI1;
+		else if (SPI_Conf->SPIx == SPI2)
+			NVIC_Enable_SPI2;
+	}
+	else
+	{
+		if (SPI_Conf->SPIx == SPI1)
+			NVIC_Disable_SPI1;
+		else if (SPI_Conf->SPIx == SPI2)
+			NVIC_Disable_SPI2;
+	}
+
+
+	SPI_GPIO_PINs(SPI_Conf);
+
+
+	// Upload Configuration To CR1 and CR2
+	SPI_Conf->SPIx->CR1 = (uint16_t)tempCR1;
+	SPI_Conf->SPIx->CR2 = (uint16_t)tempCR2;
+	tempCR1 |= (1 << SPE_BIT_Pos);
+	SPI_Conf->SPIx->CR1 = (uint16_t)tempCR1;
+
+}
+
+void SPI_RX(SPI *SPI_Conf, uint16_t *RX_Buffer)
+{
+
+	// Pooling Untill Reccieve Buffer not empty
+	while (! SPI_Conf->SPIx->SR & 1 << RXNE_BIT_Pos);
+
+	*RX_Buffer = SPI_Conf->SPIx->DR;
+}
+
+
+
+void SPI_TX(SPI *SPI_Conf, uint16_t *TX_Buffer)
+{
+
+
+	// Pooling Untill Transmit Buffer empty
+	while (! SPI_Conf->SPIx->SR & 1 << TXE_BIT_Pos);
+
+	SPI_Conf->SPIx->DR = *TX_Buffer;
+}
+
+
+void SPI_RX_TX(SPI *SPI_Conf, uint16_t *TX_RX_Buffer)
+{
+	// Pooling Untill Transmit Buffer empty
+	while (! SPI_Conf->SPIx->SR & 1 << TXE_BIT_Pos);
+
+	SPI_Conf->SPIx->DR = *TX_RX_Buffer;
+
+	// Pooling Untill Reccieve Buffer not empty
+	while (! SPI_Conf->SPIx->SR & 1 << RXNE_BIT_Pos);
+
+	*TX_RX_Buffer = SPI_Conf->SPIx->DR;
+}
+
+
+void SPI_deINIT(SPI *SPI_Conf)
+{
+
+	if (SPI_Conf->SPIx == SPI1)
+	{
+		RCC_SPI1_CLK_DIS;
+		NVIC_Disable_SPI1;
+	}
+	else if (SPI_Conf->SPIx == SPI2)
+	{
+		RCC_SPI2_CLK_DIS;
+		NVIC_Disable_SPI2;
+	}
+}
+
+
+
+/*
+ * =======================================================================================
+ * 										Generic Functions
+ * =======================================================================================
+*/
+
+
+static void SPI_GPIO_PINs(SPI *SPI_Conf)
+{
+	GPIO_PinConfig_t SPI_PIN;
+	if (SPI_Conf->SPIx == SPI1)
+	{
+		// PA5 SCK
+		// PA6 MISO
+		// PA7 MOSI
+		SPI_PIN.GPIOx = GPIOA;
+		if (SPI_Conf->Master_Slave_Mode == SPI_Master)
+		{
+			// Init CLK Pin
+			SPI_PIN.GPIO_PinNumber = GPIO_PIN5;
+			SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+			SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+			GPIO_INIT(&SPI_PIN);
+
+
+			// Init MISO Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN6;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+			// Init MOSI Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN7;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+				SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+		}
+		else if (SPI_Conf->Master_Slave_Mode == SPI_Slave)
+		{
+			// Init CLK Pin
+			SPI_PIN.GPIO_PinNumber = GPIO_PIN5;
+			SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+			GPIO_INIT(&SPI_PIN);
+
+
+			// Init MISO Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN6;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+				SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+			// Init MOSI Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN7;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+		}
+	}
+	else if (SPI_Conf->SPIx == SPI2)
+	{
+		// PB13 SCK
+		// PB14 MISO
+		// PB15 MOSI
+
+		SPI_PIN.GPIOx = GPIOB;
+		if (SPI_Conf->Master_Slave_Mode == SPI_Master)
+		{
+			// Init CLK Pin
+			SPI_PIN.GPIO_PinNumber = GPIO_PIN13;
+			SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+			SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+			GPIO_INIT(&SPI_PIN);
+
+
+			// Init MISO Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN14;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+			// Init MOSI Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN15;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+				SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+		}
+		else if (SPI_Conf->Master_Slave_Mode == SPI_Slave)
+		{
+			// Init CLK Pin
+			SPI_PIN.GPIO_PinNumber = GPIO_PIN13;
+			SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+			GPIO_INIT(&SPI_PIN);
+
+
+			// Init MISO Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN14;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP;
+				SPI_PIN.GPIO_OUTPUT_Speed = GPIO_SPEED_10M;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+
+			// Init MOSI Pin
+			if (SPI_Conf->Communiaction_Mode == Full_Duplex)
+			{
+				SPI_PIN.GPIO_PinNumber = GPIO_PIN15;
+				SPI_PIN.GPIO_MODE = GPIO_MODE_INPUT_FLO;
+				GPIO_INIT(&SPI_PIN);
+			}
+			else if (SPI_Conf->Communiaction_Mode == Half_Duplex)
+			{
+				// To Do
+			}
+		}
+	}
+
+}
+
--- Drivers/STM32F103C6_MCAL_Drivers/SPI/STM32F103C6_SPI_Driver.h
+++ Drivers/STM32F103C6_MCAL_Drivers/SPI/STM32F103C6_SPI_Driver.h
@@ -6,3 +6,184 @@
  *
  ******************************************************************************
 **/
+#ifndef _SPI_DRIVER_H_
+#define _SPI_DRIVER_H_
+
+
+#include "../STM32F103X6.h"
+#include "../GPIO/STM32F103C6_GPIO_Driver.h"
+#include "../RCC/STM32F103C6_RCC_Driver.h"
+
+
+// CR1 Bits Pos
+#define CPHA_BIT_Pos 				0
+#define CPOL_BIT_Pos 				1
+#define MSTR_BIT_Pos  				2
+#define BR_BITS_Pos					3 //[3-5]
+#define SPE_BIT_Pos   				6
+#define LSBFIRST_BIT_Pos			7
+#define SSM_BIT_Pos					9
+#define RXONLY_BIT_Pos				10
+#define DFF_BIT_Pos					11
+#define BIDIOE_BIT_Pos				14
+#define BIDIMODE_BIT_Pos			15
+
+
+// CR2 Bits Pos
+#define TXEIE_BIT_Pos				7
+#define RXNEIE_BIT_Pos				6
+#define ERRIE_BIT_Pos				5
+
+// SR Bits Pos
+#define RXNE_BIT_Pos				0
+#define TXE_BIT_Pos					1
+#define BUSY_BIT_Pos 				7
+
+
+/**********************************Reference Macros*************************************/
+//@ref SPIx
+// SPI1           for using SPI1
+// SPI2           for using SPI2
+
+
+//@ref SPI_Mode
+
+typedef enum
+{
+	Half_Duplex,
+	Full_Duplex
+}Comm_Mode;
+
+
+//@ref SPI_Master_Slave
+// Bit 2 int CR1 Reg MSTR Identify Master Or Slave
+typedef enum
+{
+	SPI_Slave,
+	SPI_Master
+}Device_Mode;
+
+//@ref SPI_Frame_Size
+typedef enum
+{
+	SPI_BIT_8,
+	SPI_BIT_16
+}Data_Size;
+
+//@ref SPI_SCLK
+typedef enum
+{
+	SPI_BAURDRATEPRESCALLER_2   = 0    ,
+	SPI_BAURDRATEPRESCALLER_4   = 0b001,
+	SPI_BAURDRATEPRESCALLER_8   = 0b010,
+	SPI_BAURDRATEPRESCALLER_16  = 0b011,
+	SPI_BAURDRATEPRESCALLER_32  = 0b100,
+	SPI_BAURDRATEPRESCALLER_64  = 0b101,
+	SPI_BAURDRATEPRESCALLER_128 = 0b110,
+	SPI_BAURDRATEPRESCALLER_256 = 0b111
+}BaudRate_Prescaller;
+
+
+
+//@ref SPI_IRQ
+typedef enum
+{
+	SPI_IRQ_NONE,      							// Disable IRQ
+	SPI_IRQ_TXEIE 	= 	1 << TXEIE_BIT_Pos,		// Enable IRQ When Transmitt Buffer empty
+	SPI_IRQ_RXNEIE 	=  	1 << RXNEIE_BIT_Pos,	// Enable IRQ When Reciever Buffer not Empty
+	SPI_IRQ_ERRIE  	=	1 << ERRIE_BIT_Pos		// Enable Interrupt IRQ
+
+}IRQ_EN;
+
+/***************************************************************************************/
+
+
+typedef struct
+{
+	SPI_Periphral        *SPIx				;			// Specifies Which SPI Use.
+														// This parameter must be set based on @ref SPIx
+
+	Comm_Mode			 Communiaction_Mode ;			// Specifies Which SPI Mode.
+														// This parameter must be set based on @ref SPI_Mode
+
+	Device_Mode			 Master_Slave_Mode	;			// Specifies Master || Slave.
+														// This parameter must be set based on @ref SPI_Master_Slave
+
+	Data_Size			 Frame_Size			;			// Specifies 8BIT Or 16BIT Frame.
+														// This parameter must be set based on @ref SPI_Frame_Size
+
+	BaudRate_Prescaller	 Prescaller			;			// Specifies SCLK Freq.
+														// This parameter must be set based on @ref SPI_SCLK
+
+
+
+	IRQ_EN				 IRQ				;			// Specifies IRQ En Or Dis.
+														// This parameter must be set based on @ref SPI_IRQ
+
+	void(*IRQ_CALL)()						;			// This Function Will Call if you Enable IRQ Of Periphral
+
+}SPI;
+
+
+
+/*
+ * =======================================================================================
+ * 										APIs
+ * =======================================================================================
+*/
+
+
+
+/**================================================================
+ * @Fn                 	- SPI_MASTER_INIT
+ * @brief				- You Must Call This Func Before Any Using APIS
+ * @param [in]			- Pointer to your SPI
+ *
+ * @retval				- NONE
+ * Note					- NONE
+*/
+void SPI_MASTER_INIT(SPI *SPI_Conf);
+
+/**================================================================
+ * @Fn                 	- SPI_RX
+ * @brief				- To Recieve From RX Buffer
+ * @param [in]			- Pointer to your SPI and Your Buffer to reciev
+ *
+ * @retval				- NONE
+ * Note					- NONE
+*/
+void SPI_RX(SPI *SPI_Conf, uint16_t *RX_Buffer);
+
+/**================================================================
+ * @Fn                 	- SPI_TX
+ * @brief				- To Transmit Your TX Buffer
+ * @param [in]			- Pointer to your SPI and Your Buffer that Need to Transmit
+ *
+ * @retval				- NONE
+ * Note					- NONE
+*/
+void SPI_TX(SPI *SPI_Conf, uint16_t *TX_Buffer);
+
+/**================================================================
+ * @Fn                 	- SPI_RX_TX
+ * @brief				- To Transmit and recieve at the same time
+ * @param [in]			- Pointer to your SPI and Your Buffer that Need to Transmit and Store in it The RX Buffer
+ *
+ * @retval				- NONE
+ * Note					- NONE
+*/
+void SPI_RX_TX(SPI *SPI_Conf, uint16_t *TX_RX_Buffer);
+
+/**================================================================
+ * @Fn                 	- SPI_deINIT
+ * @brief				- Disable Periphral
+ * @param [in]			- Pointer to your SPI
+ *
+ * @retval				- NONE
+ * Note					- NONE
+*/
+void SPI_deINIT(SPI *SPI_Conf);
+
+
+#endif
+
